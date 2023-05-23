014cbcb4d64ccf6f8c0ba843bd63b45cba0969da
Reynaldo Pereira Heredia <darkdeymon04@gmail.com>
2023-05-22 21:33:25 -0400

    avance 2

--- apps/empleado/views.py
+++ apps/empleado/views.py
@@ -1,3 +1,42 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+class ListQuejasView(ListView):
+	model = User
+	template_name = 'auth/list_user.html'
+	form_class = search_form
+	paginate_by = 10
+	def get_context_data(self, **kwargs):
+		context = super().get_context_data(**kwargs)
+		if 'form' not in context:
+			context['form'] = self.form_class()
+		if self.request.GET:
+			context['form'] = self.form_class(self.request.GET)
+			form = self.form_class(self.request.GET)
+			if form.is_valid():
+				if form.cleaned_data['search']=='':
+					context['searchdata'] = None
+				else:
+					context['searchdata'] = form.cleaned_data['search']
+		return context
+	def get_queryset(self):
+		search = None
+		if self.request.method == "GET":
+			form = self.form_class(self.request.GET)
+			if form.is_valid():
+				search = form.cleaned_data['search']
+		if (search):
+			return self.model.objects.annotate(
+					search=SearchVector(
+						Cast('id', CharField()),
+						'username',
+						'first_name',
+						'last_name',
+						Cast('email', CharField())
+					)
+				).filter(
+					search=search,
+				).order_by('id')
+		else:
+			return self.model.objects.all().order_by('id')
