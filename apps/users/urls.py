51f525b8d3616a3a72ddda7094167fd940bf24a7
Reynaldo Pereira heredia <darkdeymon04@gmail.com>
2025-03-16 15:40:33 -0400

    ¨versdiones¨

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -31,7 +31,7 @@ router.register(r'user', UserViewSet)
 router.register(r'metricas', MetricasViewSet)
 
 class DateConverter:
-    regex = '\d{4}-\d{1,2}-\d{1,2}'
+    regex = r'^\d{4}-\d{1,2}-\d{1,2}$'
     format = '%Y-%m-%d'
 
     def to_python(self, value):
